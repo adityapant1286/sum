4f0857767b5f4bc73d05f82afa09b72f2d3a196e
Aditya.Pant <Aditya.Pant@health.telstra.com>
2018-05-18 15:13:49 +1000

    java 8

--- src/main/java/com/sum/App.java
+++ src/main/java/com/sum/App.java
@@ -4,75 +4,86 @@ import java.math.BigInteger;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
-/**
- * Hello world!
- */
 public class App {
 
     public static void main(String[] args) {
-//        add(5, 2);
-        sum_v2(5, 2);
+
+        final App app = new App();
+
+        app.sumOfConsecutive(5, 2, 10); // Sum = 21 | TimeTaken = 74 millis
+        app.sumOfConsecutive(10000000, 200, 10); // Sum = 4965134354 | TimeTaken = 30689 millis (10 digits limit)
+        app.sumOfConsecutive(1000000, 200, 10); // Sum = 4876116127 | TimeTaken = 2619 millis (10 digits limit)
     }
 
-    static void sum(int n, int c) {
-        BigInteger productElements = BigInteger.ONE;
-        BigInteger sum = BigInteger.ZERO;
-        long firstProductElement = 1;
-        //System.out.println(new Date());
-        for (int i = 1; i <= c; i++) {
-            System.out.println(i);
-            productElements = productElements.multiply(BigInteger.valueOf(i));
-            sum = sum.add(productElements);
+    public void sumOfConsecutive(Integer n, Integer c, int noOfDigits) {
+
+        boolean nIsGreaterThanC = true;
+
+        if (n.compareTo(c) < 0) { // if n is less than c, then use available numbers
+            c = n;
+            nIsGreaterThanC = false;
         }
-        System.out.println(sum);
-        for (long i = ++c; i < n; i++) {
-            productElements = productElements.divide(BigInteger.valueOf(firstProductElement))
-                                                .multiply(BigInteger.valueOf(i));
-            sum = sum.add(productElements);
-            firstProductElement += 1;
-        }
-        String sumStr = sum.toString();
-        sum = sumStr.length() > 10 ? BigInteger.valueOf(Long.valueOf(sumStr.substring(0, 10))) : sum;
-        //System.out.println(new Date());
-        System.out.println(sum);
 
+        final Calc calc = new Calc();
+        long start = System.currentTimeMillis();
+        // 1...c
+        IntStream.rangeClosed(1, c).mapToObj(BigInteger::valueOf).forEach(calc::productSum);
+
+        if (nIsGreaterThanC)
+            // ++c...n
+            LongStream.range(++c, n).mapToObj(BigInteger::valueOf).forEach(calc::divideProductSum);
+
+        final String sum = adjustToLimit(calc.getSum().toString(), noOfDigits);
+
+        displaySum(sum, start);
     }
 
+    private String adjustToLimit(String sum, int limit) { return sum.length() > limit ? sum.substring(0, limit) : sum; }
 
-    static void sum_v2(Integer n, Integer c) {
+    private void displaySum(String sum, long start) { System.out.println(String.format("Sum = %s | TimeTaken = %s", sum, timeTaken(start))); }
 
-        IntStream.rangeClosed(1, c).mapToObj(BigInteger::valueOf).forEach(Calc::productThenSum);
-        LongStream.range(++c, n).mapToObj(BigInteger::valueOf).forEach(Calc::divideProductSum);
-
-
-        String sum = Calc.getSum().toString();
-
-        System.out.println(sum.length() > 10 ? sum.substring(0, 10) : sum);
-
-    }
+    private String timeTaken(long start) { return String.format("%d millis", (System.currentTimeMillis() - start)); }
 
 
-    private static class Calc {
+    /**
+     * <pre>
+     * This class will be used to calculate multiplication
+     * and sum
+     * </pre>
+     */
+    private class Calc {
 
-        private static BigInteger product = BigInteger.ONE;
-        private static BigInteger sum = BigInteger.ZERO;
-        private static BigInteger holder = BigInteger.ONE;
+        private BigInteger product = BigInteger.ONE;
+        private BigInteger sum = BigInteger.ZERO;
+        private BigInteger holder = BigInteger.ONE;
 
-        static void productThenSum(BigInteger prod) {
+
+        void productSum(BigInteger prod) {
             product(prod);
             add(product);
         }
 
-        static void divideProductSum(BigInteger bigInteger) {
+        void divideProductSum(BigInteger bigInteger) {
             product = product.divide(holder).multiply(bigInteger);
             holder = holder.add(BigInteger.ONE);
             add(product);
         }
 
-        static BigInteger getSum() { return sum; }
+        BigInteger getSum() { return sum; }
 
-        private static void product(BigInteger prod) { product = product.multiply(prod); }
+        /**
+         * <pre>
+         * As per Java documentation {@link BigInteger#multiplyKaratsuba(BigInteger, BigInteger)}
+         * when the result exceeds the {@link BigInteger#KARATSUBA_THRESHOLD}, the
+         * Karatsuba algorithm executes which has {@code O(n^(log2(3))), or O(n^1.585)} complexity.
+         *
+         * If the result exceeds the {@link BigInteger#TOOM_COOK_THRESHOLD}, the
+         * 3-way Toom-Cook algorithm executes which has {@code O(n^1.465)} complexity.
+         * </pre>
+         * @param prod - input
+         */
+        private void product(BigInteger prod) { product = product.multiply(prod); }
 
-        private static void add(BigInteger bigInteger) { sum = sum.add(bigInteger); }
+        private void add(BigInteger bigInteger) { sum = sum.add(bigInteger); }
     }
 }
